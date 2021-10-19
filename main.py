faa354f851e2799514d9da0c3687f81e78ba5a1a
YMOHANTY <yasowant@live.com>
2021-10-18 23:37:49 -0400

    new Snake class

--- main.py
+++ main.py
@@ -7,31 +7,25 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-# t1 = Turtle("square")
-# t2 = Turtle("square")
-# t3 = Turtle("square")
-#
-# t1.color("white")
-# t2.color("green")
-# t3.color("yellow")
-#
-# #print(t1.turtlesize())
-# #t1.shapesize(1, 3, 1)
-#
-# tup = t1.position()
-# t2.goto(t1.xcor()-20,t1.ycor())
-# t3.goto(t2.xcor()-20,t2.ycor())
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
 
 flag = True
 
 while flag:
     screen.update()
-    sleep(0.5)
+    sleep(0.1)
     snake.move()
 
 
-
+screen.update()
 screen.exitonclick()
 
--- snake.py
+++ snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle, Screen
 
 START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
-
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snake_segments = []
         self.create_snake()
+        self.snake_head = self.snake_segments[0]
 
     def create_snake(self):
         for _ in START_POSITION:
@@ -19,5 +24,20 @@ class Snake:
     def move(self):
         for _ in range(len(self.snake_segments) - 1, 0, -1):
             self.snake_segments[_].goto(self.snake_segments[_ - 1].xcor(), self.snake_segments[_ - 1].ycor())
-        self.snake_segments[0].left(90)
-        self.snake_segments[0].forward(20)
+        self.snake_head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
