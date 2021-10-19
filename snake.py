01e3a3ac6e7cc96f66663e4820948651df83585c
YMOHANTY <yasowant@live.com>
2021-10-18 23:09:32 -0400

    new Snake class

--- snake.py
+++ snake.py
@@ -1,20 +1,23 @@
 from turtle import Turtle, Screen
 
+START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 
-start_position = [(0, 0), (-20, 0), (-40, 0)]
-snake = []
 
 class Snake:
     def __init__(self):
-       for _ in start_position:
-           t1 = Turtle("square")
-           t1.color("white")
-           t1.penup()
-           t1.goto(_)
-           snake.append(t1)
+        self.snake_segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for _ in START_POSITION:
+            t1 = Turtle("square")
+            t1.color("white")
+            t1.penup()
+            t1.goto(_)
+            self.snake_segments.append(t1)
 
     def move(self):
-        for _ in range(len(snake) - 1, 0, -1):
-            snake[_].goto(snake[_ - 1].xcor(), snake[_ - 1].ycor())
-        snake[0].left(90)
-        snake[0].forward(20)
+        for _ in range(len(self.snake_segments) - 1, 0, -1):
+            self.snake_segments[_].goto(self.snake_segments[_ - 1].xcor(), self.snake_segments[_ - 1].ycor())
+        self.snake_segments[0].left(90)
+        self.snake_segments[0].forward(20)
